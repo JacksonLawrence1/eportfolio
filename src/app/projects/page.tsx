609c01e47c3a2aaef33c130247383588b3267dcd
Jackson Lawrence <fi19710@bristol.ac.uk>
2023-10-26 18:30:47 +0100

    Sort Button and small refactoring

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,42 +1,65 @@
-'use client'
+"use client";
 
 import Back from "@/components/back";
 import Project from "./project";
+import { useState } from "react";
 
 import { BsSortDown } from "react-icons/bs";
+import { FaCheck } from "react-icons/fa";
 
-import { projects } from "./projectStore";
+import { getProjects } from "./projectStore";
 
-const SortByScale = () => {
-  console.log("sorted by scale");
-}
+const Projects = () => {
+  let [projects, setProjects] = useState(getProjects(1));
+  let [sortIndex, setSortIndex] = useState(1);
+  let [open, setOpen] = useState(false);
 
-const SortByDate = () => {
-  console.log("sorted by date");
-}
+  const toggleOpen = (event : React.MouseEvent) => {
+    event.preventDefault();
+    setOpen(!open);
+  };
 
-const Projects = () => (
-  <div className="flex flex-col justify-center items-center gap-8 pt-20 pb-8">
-    <div className="flex justify-center items-center gap-4">
-      <h1 className="text-5xl text-secondary">Projects</h1>
-      <details>
-        <summary className="icon">
-          <BsSortDown />
-        </summary>
-        <ul className="dropdown-content text-lg">
-          <li className="px-2 text-accent-1 mt-1 py-1 text-xl">Sort By</li>
-          <li className="list-content"><a onClick={SortByScale}>Project Scale</a></li>
-          <li className="list-content"><a onClick={SortByDate}>Completion Date</a></li>
-        </ul>
-      </details>
+  const sortProjects = (index: number) => {
+    if (index === sortIndex) return;
+
+    const sortedProjects = getProjects(index);
+    setProjects(sortedProjects);
+    setSortIndex(index);
+  };
+
+  const dropdown = (name: string, index: number) => (
+    <li className="list-content" onClick={() => sortProjects(index)}>
+      <div className="flex justify-between gap-4 items-center">
+        {name} {sortIndex === index && <FaCheck />}
+      </div>
+    </li>
+  );
+
+  return (
+    <div className="flex flex-col justify-center items-center gap-8 pt-20 pb-8">
+      <div className="flex justify-center items-center gap-4">
+        <h1 className="text-5xl text-secondary">Projects</h1>
+        <details open={open} onClick={toggleOpen}>
+          <summary className={open ? "icon-enabled" : "icon"} onClick={toggleOpen}>
+            <BsSortDown />
+          </summary>
+          <ul className="dropdown-content text-lg">
+            <li className="px-2 text-accent-1 mt-1 py-1 text-xl">Sort By</li>
+            {dropdown("Date", 0)}
+            {dropdown("Scale", 1)}
+            {dropdown("Title", 2)}
+            {dropdown("Team Size", 3)}
+          </ul>
+        </details>
+      </div>
+      <div className="flex flex-wrap items-center justify-center gap-y-4">
+        {projects.map((project) => (
+          <Project key={project.id} {...project} />
+        ))}
+      </div>
+      <Back />
     </div>
-    <div className="flex flex-wrap items-center justify-center gap-y-4">
-      {projects.map((project) => (
-        <Project key={project.id} {...project} />
-      ))}
-    </div>
-    <Back />
-  </div>
-);
+  );
+};
 
 export default Projects;
--- src/app/projects/projectStore.tsx
+++ src/app/projects/projectStore.tsx
@@ -1,4 +1,4 @@
-export const projects = [
+const p = [
   {
     title: "Warcraft Portfolio",
     id: "wp",
@@ -6,7 +6,7 @@ export const projects = [
     image: "/images/warcraft-portfolio.png",
     description:
       "Warcraft Portfolio aims to display useful character achievements for individuals in the game, World of Warcraft. This includes things such as their progress across different raids and their performance ranked against others. By using the Blizzard and WarcraftLogs API, Warcraft Portfolio pulls and displays data directly from the World of Warcraft database.",
-    skills: ["Svelte", "SvelteKit", "JavaScript", "TailwindCSS", "APIs"],
+    skills: ["Svelte", "JavaScript", "SvelteKit", "TailwindCSS", "APIs"],
     buttons: [
       {
         name: "Website",
@@ -25,7 +25,7 @@ export const projects = [
     image: "/images/sensitivity-converter.png",
     description:
       "A very simple tool designed to help players easily convert their mouse sensitivity values between games. This small project was mainly created to help me learn the basics of React and Tailwind, but I still use it when swapping sensitivity between games.",
-    skills: ["JavaScript", "React", "NextJS", "TailwindCSS"],
+    skills: ["React", "JavaScript", "NextJS", "TailwindCSS"],
     buttons: [
       {
         name: "Website",
@@ -44,7 +44,7 @@ export const projects = [
     image: "/images/morbent-manor.png",
     description:
       "Developed as part of my final year thesis at university. Morbent Manor is a 2D roguelike where the player must traverse through a series of levels, gaining intriguing powerups modelled around MOBA style gameplay. I achieved a high first (80%) for this project, involving over 10,000+ lines of C# code, 224 unit tests and 35+ custom made 2D sprite sheets drawn and animated using Aseprite. Please contact me if you would like to see the source code or the dissertation itself as I am planning on releasing it publicly on Steam.",
-    skills: ["C#", "Unity", "OOP", "Animation", "State Machines"],
+    skills: ["Unity", "C#", "OOP", "Animation", "State Machines", "TDD"],
     buttons: [
       {
         name: "View Demo",
@@ -62,8 +62,8 @@ export const projects = [
     date: "Jan. 2022 -> May. 2022",
     image: "/images/kf.jpg",
     description:
-      "Working alongside 5 other students, we created a 2D multiplayer game in Unity as part of the degree's Year 3 games project. Kitchen Feud is a chaotic 3D cooking game where two teams compete to run the best resturant. I was mainly responsible for working on the online interaction in the game, through RPC calls using the Photon plugin. This included interactions such as rejoining after having disconnected, and syncing the game state across all players.",
-    skills: ["C#", "Unity", "OOP", "Multiplayer", "RPCs"],
+      "Working alongside 5 other students, we created a 2D multiplayer game in Unity as part of the degree's Year 3 games project. Kitchen Feud is a chaotic 3D cooking game where two teams compete to run the best restaurant. I was mainly responsible for helping create 3D assets from scratch using Maya, and working on the online interaction in the game, through the use of RPC calls. This included interactions such as rejoining after having disconnected, and smoothly syncing the game state across players.",
+    skills: ["Unity", "C#", "OOP", "Multiplayer", "Networking", "3D Modeling", "Scrum"],
     buttons: [
       {
         name: "Play Here",
@@ -101,7 +101,7 @@ export const projects = [
     image: "/images/pilgrim.jpg",
     description:
       "Alongside 4 other students, we created a proof-of-concept Android App called Ale Trail which allowed users to track their drinking progress as they went on a pub crawl, potentially making them eligible to win prizes for their performance. I was responsible for working on backend functionality, including the database run on AWS, using MySQL. The app itself used SQL queries to fetch relevant data from the database. Since the product was bespokely designed, please contact me if you would like to see/hear further details.",
-    skills: ["Kotlin", "AWS", "SQL", "MySQL", "Android"],
+    skills: ["Android-Studio", "Kotlin", "AWS", "MySQL", "Agile"],
     buttons: [
       {
         name: "Contact",
@@ -129,3 +129,39 @@ export const projects = [
     ],
   },
 ];
+
+
+const SortProject = (arr: Array<string>): Array<any> => {
+  if (arr.length !== p.length) {
+    throw new Error("Ensure all projects are included");
+  }
+
+  // return p sorted by arr of ids
+  const sorted = arr.map((id) => {
+    if (!p.find((project) => project.id === id)) {
+      throw new Error("Invalid id");
+    }
+    return p.find((project) => project.id === id);
+  });
+
+  return sorted;
+};
+
+// 0 = default (date)
+// 1 = scale
+// 2 = title
+// 3 = team size
+export const getProjects = (sortIndex: number = 0, reverse: boolean = false) => {
+  let projects = [...p]
+
+  if (sortIndex === 1) {
+    // sort by subjective scale of project
+    projects = SortProject(["wp", "sc", "mm", "kf", "re", "at", "gc"]);
+  } else if (sortIndex === 2) {
+    projects.sort((a, b) => (a.title > b.title) ? 1 : -1);
+  } else if (sortIndex === 3) {
+    // sort by amount of people involved
+    projects = SortProject(["kf", "at", "mm", "wp", "re", "gc", "sc"]);
+  }
+  return reverse ? projects.reverse() : projects;
+};

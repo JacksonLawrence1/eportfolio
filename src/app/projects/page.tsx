c495584a25c7d5e247a936a187e12f932c516f7b
Jackson Lawrence <fi19710@bristol.ac.uk>
2023-10-26 15:26:33 +0100

    sort wip

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Back from "@/components/back";
 import Project from "./project";
 
@@ -5,24 +7,28 @@ import { BsSortDown } from "react-icons/bs";
 
 import { projects } from "./projectStore";
 
-const OpenSortMenu = () => {
-  return (
-    <div>
+const SortByScale = () => {
+  console.log("sorted by scale");
+}
 
-    </div>
-  )
+const SortByDate = () => {
+  console.log("sorted by date");
 }
 
 const Projects = () => (
   <div className="flex flex-col justify-center items-center gap-8 pt-20 pb-8">
     <div className="flex justify-center items-center gap-4">
       <h1 className="text-5xl text-secondary">Projects</h1>
-      <div className="icon group">
-        <BsSortDown />
-        <span className="icon-tooltip group-hover:scale-100">
-          Sort By (WIP)
-        </span>
-      </div>
+      <details>
+        <summary className="icon">
+          <BsSortDown />
+        </summary>
+        <ul className="dropdown-content text-lg">
+          <li className="px-2 text-accent-1 mt-1 py-1 text-xl">Sort By</li>
+          <li className="list-content"><a onClick={SortByScale}>Project Scale</a></li>
+          <li className="list-content"><a onClick={SortByDate}>Completion Date</a></li>
+        </ul>
+      </details>
     </div>
     <div className="flex flex-wrap items-center justify-center gap-y-4">
       {projects.map((project) => (
